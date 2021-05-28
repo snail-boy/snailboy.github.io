08daddc6c721677e439a88c956fab7f0126bbbf3
websmallrabbit <wk_web@163.com>
2021-05-28 13:52:50 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# 1.0.0 (2021-05-27)
+
+
+### Bug Fixes
+
+* **ui:** 增加自动提交记录 ([2a3be21](https://github.com/websmallrabbit/snail-blog/commit/2a3be218003b62351ab983506cd4977a30ad2aba))
+* **ui:** 解决github静态资源访问不到 ([ff8d32f](https://github.com/websmallrabbit/snail-blog/commit/ff8d32f39d82268f4ad9c42999a778ac5c038560))
+* **增加代码提交规范配置,cz:** cz ([cd40172](https://github.com/websmallrabbit/snail-blog/commit/cd40172f216d190000f8d213d867848bfaf8b481))
+
+
+
+# 1.0.0 (2021-05-27)
+
+
+### Bug Fixes
+
+* **增加代码提交规范配置,cz:** cz ([cd40172](https://github.com/websmallrabbit/snail-blog/commit/cd40172f216d190000f8d213d867848bfaf8b481))
+
+
+
---
+++ deploy.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# deploy to github pages
+# echo 'blog.webrabbit.top' > CNAME
+
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='deploy'
+#   githubUrl=git@github.com:websmallrabbit/snail-blog.git
+# else
+#   msg='来自github actions的自动部署'
+#   githubUrl=https://websmallrabbit:${GITHUB_TOKEN}@github.com/websmallrabbit/snail-blog.git
+#   git config --global user.name "websmallrabbit"
+#   git config --global user.email "wk_web@qq.com"
+# fi
+# git init
+# git add -A
+# git commit -m "${msg}"
+# git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
+
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:websmallrabbit/snail-blog.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://websmallrabbit:${GITHUB_TOKEN}@github.com/websmallrabbit.github.io.git
+  git config --global user.name "websmallrabbit"
+  git config --global user.email "wk_web@qq.com"
+fi
+git init
+git add -A
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github
+
+
+# deploy to coding pages
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
+
+
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:wksnail/wksnail/snail-blog.git
+# else
+#   codingUrl=https://wk_web:${CODING_TOKEN}git@e.coding.net:wksnail/wksnail/snail-blog.git
+    
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
+
+cd -
+rm -rf docs/.vuepress/dist
