e671597f4e54f8c86564a52cfd677fc91d8d3666
websmallrabbit <wk_web@163.com>
2021-05-29 12:01:22 +0800

    feat(ui): codingpeizhi

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run build
+# npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -27,19 +27,19 @@ cd docs/.vuepress/dist
 # git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
 
-if [ -z "$GITHUB_TOKEN" ]; then
-  msg='deploy'
-  githubUrl=git@github.com:websmallrabbit/websmallrabbit.github.io.git
-else
-  msg='来自github actions的自动部署'
-  githubUrl=https://websmallrabbit:${GITHUB_TOKEN}@github.com/websmallrabbit/websmallrabbit.github.io.git
-  git config --global user.name "websmallrabbit"
-  git config --global user.email "wk_web@qq.com"
-fi
-git init
-git add -A
-git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+# if [ -z "$GITHUB_TOKEN" ]; then
+#   msg='deploy'
+#   githubUrl=git@github.com:websmallrabbit/websmallrabbit.github.io.git
+# else
+#   msg='来自github actions的自动部署'
+#   githubUrl=https://websmallrabbit:${GITHUB_TOKEN}@github.com/websmallrabbit/websmallrabbit.github.io.git
+#   git config --global user.name "websmallrabbit"
+#   git config --global user.email "wk_web@qq.com"
+# fi
+# git init
+# git add -A
+# git commit -m "${msg}"
+# git push -f $githubUrl master:gh-pages # 推送到github
 
 # test
 # deploy to coding pages
@@ -47,14 +47,14 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:websmallrabbit/snail-blog/snail-blog.git
-else
-  codingUrl=https://websmallrabbit:${CODING_TOKEN}@e.coding.net/websmallrabbit/snail-blog/snail-blog.git
-fi
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=https://git@e.coding.net:websmallrabbit/snail-blog/snail-blog.git
+# else
+#   codingUrl=https://websmallrabbit:${CODING_TOKEN}@e.coding.net/websmallrabbit/snail-blog/snail-blog.git
+# fi
 git add -A
 git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+git push -f https://git@e.coding.net:websmallrabbit/snail-blog/snail-blog.gitgit master # 推送到coding
 
 # cd -
 # rm -rf docs/.vuepress/dist
