19e7fe8254fc7be25e81fb87913a5e2d0a065af8
websmallrabbit <wk_web@163.com>
2021-05-29 11:35:20 +0800

    feat(ui): baidu

--- deploy.sh
+++ deploy.sh
@@ -47,16 +47,16 @@ git push -f $githubUrl master:gh-pages # 推送到github
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
 
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:websmallrabbit/snail-blog/snail-blog.git
-else
-  codingUrl=https://websmallrabbit:${CODING_TOKEN}git@e.coding.net/websmallrabbit/snail-blog/snail-blog.git
-fi
-git add -A
-git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:websmallrabbit/snail-blog/snail-blog.git
+# else
+#   codingUrl=https://websmallrabbit:${CODING_TOKEN}git@e.coding.net/websmallrabbit/snail-blog/snail-blog.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
-cd -
-rm -rf docs/.vuepress/dist
+# cd -
+# rm -rf docs/.vuepress/dist
 
 # test1
