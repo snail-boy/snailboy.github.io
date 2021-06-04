3277afa7e7d1339c37ec7ba625b08c6993547b80
websmallrabbit <wk_web@163.com>
2021-06-04 23:40:22 +0800

    feat(ui): 配置一些插件

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -107,4 +107,56 @@ module.exports = [
       },
     },
   ],
+  [
+    //彩带背景 先安装在配置， npm install vuepress-plugin-ribbon --save
+    "ribbon",
+    {
+      size: 90,     // width of the ribbon, default: 90
+      opacity: 0.6, // opacity of the ribbon, default: 0.3
+      zIndex: -1    // z-index property of the background, default: -1
+    }
+  ],
+  [
+    //动态标题 先安装在配置， npm install vuepress-plugin-dynamic-title --save
+    "dynamic-title",
+    {
+      showIcon: "/logo.png",
+      showText: "(/≧▽≦/)你来啦！",
+      hideIcon: "/failure.ico",
+      hideText: "(●—●)喔哟，崩溃啦！",
+      recoverTime: 2000
+    }
+  ],
+  ['meting', {
+    // metingApi: "https://meting.sigure.xyz/api/music",
+    meting: {
+      // 网易
+      server: "netease",
+      // 读取歌单
+      type: "playlist",
+      mid: "13529775",
+    },          
+    // 不配置该项的话不会出现全局播放器
+    aplayer: {
+      // 吸底模式
+      fixed: true,
+      mini: true,
+      // 自动播放
+      autoplay: true,
+      // 歌曲栏折叠
+      listFolded:true,
+      // 颜色
+      theme: '#f9bcdd',
+      // 播放顺序为随机
+      order: 'random',
+      // 初始音量
+      volume: 0.1,
+      // 关闭歌词显示
+      lrcType: 3
+    },
+    mobile :{
+      // 手机端去掉cover图
+      cover: false,
+    }
+  }]
 ]
