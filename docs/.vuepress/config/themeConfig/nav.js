5467aadbb357f29ac65f0abcdd4da7da4e4b644d
websmallrabbit <wk_web@163.com>
2021-06-10 17:24:28 +0800

    fix(ui): 增加flutter模块

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -42,6 +42,19 @@ module.exports = [
     ],
   },
   {
+    text: 'flutter',
+    link: '/flutter/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    items: [
+      // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+      {
+        text: 'flutter基础知识',
+        items: [
+          { text: 'PageView学习', link: '/pages/43a244/' },
+        ],
+      },
+    ],
+  },
+  {
     text: '页面',
     link: '/ui/',
     items: [
